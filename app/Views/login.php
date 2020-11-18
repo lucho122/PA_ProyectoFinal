2ceed836a2a253d46727ad928894224d00e1fc13
IBehemothI <54993896+IBehemothI@users.noreply.github.com>
2020-11-17 20:02:55 -0500

    Cambios Login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+<style>
+<?php include 'estilos.css'; ?>
+</style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
 </head>
+
 <?php 
 if (isset($error))
     echo $error;
 ?>
 <body>
+<div class="header">
+  <h1>Login</h1>
+  <p>Ingrese sus datos para iniciar sesión</p>
+</div>
     <form action="<?php site_url('/login'); ?>" method="post">
-        <input type="text" name="nick">
-        <input type="password" name="password">
-        <input type="submit" value="Iniciar Sesion">
+    <div class="form-control ">
+        <input type="text" name="nick" placeholder="Usuario" required autofocus><br/><br/>
+        <input type="password" name="password" placeholder="Contraseña" required autofocus><br/><br/>
+    </div>
+    <input type="submit" value="Iniciar Sesion">
     </form>
 </body>
 </html>
