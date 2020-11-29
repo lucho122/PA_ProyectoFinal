908665a4c4b77f417fd4e310b27cbd301cf5b161
Luis Vacacela <lvacacela@webcreek.com>
2020-11-29 11:50:31 -0500

    Editar Respuesta | Usuario (Con control)

--- app/Controllers/RespuestaController.php
+++ app/Controllers/RespuestaController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controllers;
 use App\Models\RespuestaModel;
+use App\Models\PreguntaModel;
 use App\Models\UsuarioModel;
 use CodeIgniter\I18n\Time;
 
@@ -40,16 +41,6 @@ class RespuestaController extends BaseController
         
         $this->session->set('usuario', ['nick' => $usuario['usunick'], 'rol' => $usuario['rolid'],
         'id' => $usuario['usuid'], 'pts' => $usuario['usupuntos']]);
-    }
-
-    public function editar($id = null) {
-        if (!parent::isAdmin())
-            return redirect()->back();
-    }
-
-    public function actualizar() {
-        if (!parent::isAdmin())
-            return redirect()->back();
     }
 
     public function adminIndex() {
@@ -109,6 +100,48 @@ class RespuestaController extends BaseController
         $this->session->markAsFlashdata('notificacion');
 
         return $this->response->redirect(base_url('/admin/respuestas'));
+    }
+
+    public function editar($id = null) {
+        if (!parent::isLogged())
+            return redirect()->back();
+        $preguntaModel = new PreguntaModel();
+        $usuarioModel = new UsuarioModel();
+        $usuario = $usuarioModel->findUser($this->session->usuario['nick']);
+        $respuestaModel = new RespuestaModel();
+        $respuesta = $respuestaModel->find($id);
+        $pregunta = $preguntaModel->getPregunta($respuesta['preid']);
+
+        $isCerrada = (new Time('now') > $pregunta->prefechacierre) ? true : false;
+
+        if($isCerrada || $usuario['usuid'] != $respuesta['usuid'])
+            return redirect()->back();
+
+        echo view('templates/head', ['titulo' => 'Editar Respuesta']);
+        echo view('templates/navbar', ['usuario' => $this->session->usuario]);  
+        echo view('pregunta/editarRespuesta', ['respuesta' => $respuesta]);
+        echo view('templates/footer');
+        
+    }
+
+    public function actualizar() {
+        if (!parent::isLogged())
+            return redirect()->back();
+    
+        $request = $this->request->getPost();
+        $respuestaModel = new RespuestaModel();
+        
+        $pregunta = trim($request['Pregunta']);
+        $fCreacion = new Time('now');
+        $id = $request['Id'];
+        $data = [
+                'rescontenido' => $request['Respuesta'],
+                'resfecha' => $fCreacion->toDateString(),
+                 ];
+
+        $respuestaModel->update($id, $data);
+
+        return $this->response->redirect(base_url('pregunta/'.$pregunta));
     }
 }
 
---
+++ app/Views/pregunta/editarRespuesta.php
@@ -0,0 +1,16 @@
+ <form action="<?= base_url('pregunta/respuesta/edit') ?>" method="post" class="form-horizontal"> 
+    <br>
+    <input type="hidden" name="Pregunta" value="<?= $respuesta['preid']; ?>">
+    <input type="hidden" name="Id" value="<?= $respuesta['resid']; ?>">
+    <div class="form-group">
+        <label class="col-sm-3 control-label">Tu respuesta</label>
+        <div class="col-sm-10">
+            <textarea class="form-control" name="Respuesta" rows="4"><?= $respuesta['rescontenido']; ?></textarea>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">                    
+            <button class="btn btn-dark  btn-circle" type="submit">Actualizar</button>
+        </div>
+    </div>         
+</form>
