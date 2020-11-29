8db5b5f340c6dfed6c748e52b2e68523eabb217d
Eddie9719 <eddieverdesoto@hotmail.com>
2020-11-29 11:20:09 -0500

    Generar Nickname

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,3 +1,4 @@
+
 <div class="container">
 <?php
 if (isset($_SESSION['errores'])) { ?>
@@ -15,7 +16,7 @@ if (isset($_SESSION['errores'])) { ?>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="PNombre">Primer Nombre *</label>
-                <input type="text" class="form-control form-control-sm" name="PNombre" value="<?= old('PNombre'); ?>">
+                <input type="text" class="form-control form-control-sm" name="PNombre" id="PNombre" value="<?= old('PNombre'); ?>">
             </div>
             <div class="form-group col-md-6">
                 <label for="SNombre">Segundo Nombre</label>
@@ -25,11 +26,18 @@ if (isset($_SESSION['errores'])) { ?>
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="PApellido">Primer Apellido *</label>
-                <input type="text" class="form-control form-control-sm" name="PApellido" value="<?= old('PApellido'); ?>">
+                <input type="text" class="form-control form-control-sm" name="PApellido" id="PApellido" value="<?= old('PApellido'); ?>">
             </div>
             <div class="form-group col-md-6">
                 <label for="SApellido">Segundo Apellido</label>
                 <input type="text" class="form-control form-control-sm" name="SApellido" value="<?= old('SApellido'); ?>">
+                <?php      
+                    if(isset($_POST['PNombre'])){
+                    $day=$_POST['PNombre'];
+                    $pieces = $day;
+                    echo $pieces;
+                    }  
+                ?>
             </div>
         </div>
         <div class="form-group">
@@ -38,7 +46,19 @@ if (isset($_SESSION['errores'])) { ?>
         </div>
         <div class="form-group">
             <label for="Nick">Nick *</label>
-            <input type="text" class="form-control form-control-sm" name="Nick" value="<?= old('Nick'); ?>">
+            
+            <input type="text" class="form-control form-control-sm" name="Nick" id="Nick" value="<?= old('Nick'); ?>">
+            <input type="button" value="Generar Nick" onclick="genNick()">
+            <script>
+                function genNick(){
+                
+                nombre = document.getElementById("PNombre").value;
+                var GenNombre = nombre.substring(0,1);
+                Genapellido  =document.getElementById("PApellido").value;
+                var NumeroAleatorio = Math.floor(Math.random()*999)+100;
+                document.getElementById("Nick").value = GenNombre+Genapellido+''+NumeroAleatorio;
+                };
+            </script>
         </div>
         <div class="form-group">
             <label for="Clave">Contraseña *</label>
@@ -69,6 +89,7 @@ if (isset($_SESSION['errores'])) { ?>
             <input type="submit" class="btn btn-dark" value="Registrarse">
         </div>
     </form>
+            
     </div>
     <br>
 </div>
